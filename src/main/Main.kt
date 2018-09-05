25a9a0d538032d1149492c7060c16b9586a4c513
Vladislav Shipugin <vladshipugin@gmail.com>
2018-09-05 23:24:56 +0300

    Инициализация проекта.

---
+++ src/main/Main.kt
@@ -0,0 +1,14 @@
+package main
+
+/**
+ * Created by v.shipugin on 05/09/2018
+ */
+fun main(args: Array<String>) {
+    Main().work()
+}
+
+class Main {
+    fun work() {
+        println("Hello, I'm Skywalker!")
+    }
+}

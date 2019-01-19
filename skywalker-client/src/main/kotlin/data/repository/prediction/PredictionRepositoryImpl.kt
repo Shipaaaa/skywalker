473dd2c6efdbbd3b142e35d2b1baf579be36e3d2
Vladislav Shipugin <vladshipugin@gmail.com>
2019-01-19 22:55:13 +0300

    [Predict] Добавил алгоритм анализа входных данных.

--- skywalker-client/src/main/kotlin/data/repository/prediction/PredictionRepositoryImpl.kt
+++ skywalker-client/src/main/kotlin/data/repository/prediction/PredictionRepositoryImpl.kt
@@ -11,6 +11,7 @@ import io.ktor.http.contentType
 import kotlinx.coroutines.runBlocking
 import java.util.*
 
+
 /**
  * Created by v.shipugin on 18/11/2018
  */
@@ -18,6 +19,13 @@ class PredictionRepositoryImpl(private val httpClient: HttpClient) : PredictionR
 
     companion object {
         private const val TAG = "PredictionRepositoryImpl"
+
+        private const val NEED_USE_SIZE = false
+
+        private const val COUNT_OF_BLOCKS = 10
+        private const val SIZE_OF_BLOCKS_IN_KB = 200
+
+        private const val SIZE_OF_SAMPLE_IN_KB = 80
 
         private val predictServiceUrl = System.getenv("PREDICT_SERVICE_URL")
             ?: "http://localhost/skywalker/predict"
@@ -38,11 +46,64 @@ class PredictionRepositoryImpl(private val httpClient: HttpClient) : PredictionR
     }
 
     private fun getSampleDataFromFile(fileEntity: FileEntity): String {
-        val bytes = fileEntity.blob
-        val encodedBlob = Base64.getEncoder().encodeToString(bytes)
+        val fileBytes = fileEntity.blob
 
-        Logger.log(TAG, "Sample data size: ${bytes.size}")
-        Logger.log(TAG, "Sample data: ${bytes.joinToString(",")}")
+        val sizeDiff = SIZE_OF_SAMPLE_IN_KB - fileBytes.size
+
+        val roundingFileBytes = if (sizeDiff > 0) {
+            ByteArray(SIZE_OF_SAMPLE_IN_KB).apply {
+                fileBytes.forEachIndexed { index, byte ->
+                    this[index] = byte
+                }
+            }
+        } else {
+            fileBytes
+        }
+
+        val sampleParts: MutableList<ByteArray> = mutableListOf()
+
+        @Suppress("ConstantConditionIf")
+        if (NEED_USE_SIZE) {
+            val totalCountOfBlocks = roundingFileBytes.size / SIZE_OF_BLOCKS_IN_KB
+            val countOfBlocks = SIZE_OF_SAMPLE_IN_KB / SIZE_OF_BLOCKS_IN_KB
+            val step = totalCountOfBlocks / countOfBlocks
+
+            var startIndex = 0
+            (0 until countOfBlocks).forEach {
+                sampleParts.add(
+                    Arrays.copyOfRange(
+                        roundingFileBytes,
+                        startIndex,
+                        startIndex + SIZE_OF_BLOCKS_IN_KB
+                    )
+                )
+                startIndex += step * SIZE_OF_BLOCKS_IN_KB
+            }
+        } else {
+            val sizeOfBlocks = SIZE_OF_SAMPLE_IN_KB / COUNT_OF_BLOCKS
+            val totalCountOfBlocks = roundingFileBytes.size / sizeOfBlocks
+            val step = totalCountOfBlocks / COUNT_OF_BLOCKS
+
+            var startIndex = 0
+            (0 until COUNT_OF_BLOCKS).forEach {
+                sampleParts.add(
+                    Arrays.copyOfRange(
+                        roundingFileBytes,
+                        startIndex,
+                        startIndex + sizeOfBlocks
+                    )
+                )
+                startIndex += step * sizeOfBlocks
+            }
+        }
+
+        var sampleBytes = ByteArray(0)
+        sampleParts.forEach { sampleBytes = sampleBytes.plus(it) }
+
+        val encodedBlob = Base64.getEncoder().encodeToString(sampleBytes)
+
+        Logger.log(TAG, "Sample data size: ${sampleBytes.size}")
+        Logger.log(TAG, "Sample data: ${sampleBytes.joinToString(",")}")
         Logger.log(TAG, "Encoded sample data: $encodedBlob")
 
         return encodedBlob
